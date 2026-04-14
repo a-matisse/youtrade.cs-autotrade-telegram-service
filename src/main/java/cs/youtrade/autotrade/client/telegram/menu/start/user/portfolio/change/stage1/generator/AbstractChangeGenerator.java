bda9312f77f83e9a0dd99e0fb49b9517d6f60664
SeeMemes <provotorov-02@mail.ru>
2026-04-14 22:33:46 +0300

    fix

--- src/main/java/cs/youtrade/autotrade/client/telegram/menu/start/user/portfolio/change/stage1/generator/AbstractChangeGenerator.java
+++ src/main/java/cs/youtrade/autotrade/client/telegram/menu/start/user/portfolio/change/stage1/generator/AbstractChangeGenerator.java
@@ -122,53 +122,55 @@ public abstract class AbstractChangeGenerator<I>
     public List<FcdSellChangePostDto> handleFile(File file) throws IOException {
         List<FcdSellChangePostDto> postList = new ArrayList<>();
         try (Workbook wb = new XSSFWorkbook(new FileInputStream(file))) {
-            Sheet sheet = wb.getSheetAt(0);
-            for (Row row : sheet) {
-                if (row.getRowNum() == 0) continue;
+            for (int i = 0; i < wb.getNumberOfSheets(); i++) {
+                Sheet sheet = wb.getSheetAt(i);
+                for (Row row : sheet) {
+                    if (row.getRowNum() == 0) continue;
 
-                String idStr = getCellString(row.getCell(0));
-                if (idStr.isEmpty()) continue;
+                    String idStr = getCellString(row.getCell(0));
+                    if (idStr.isEmpty()) continue;
 
-                String name = getCellString(row.getCell(1));
-                if (name.isEmpty()) continue;
+                    String name = getCellString(row.getCell(1));
+                    if (name.isEmpty()) continue;
 
-                String marketPriceStr = getCellString(row.getCell(2));
-                if (marketPriceStr.isEmpty())
-                    continue;
+                    String marketPriceStr = getCellString(row.getCell(2));
+                    if (marketPriceStr.isEmpty())
+                        continue;
 
-                String oldMinStr = getCellString(row.getCell(3));
-                if (oldMinStr.isEmpty()) continue;
+                    String oldMinStr = getCellString(row.getCell(3));
+                    if (oldMinStr.isEmpty()) continue;
 
-                String oldMaxStr = getCellString(row.getCell(4));
-                if (oldMaxStr.isEmpty()) continue;
+                    String oldMaxStr = getCellString(row.getCell(4));
+                    if (oldMaxStr.isEmpty()) continue;
 
-                String oldBaseStr = getCellString(row.getCell(5));
-                if (oldBaseStr.isEmpty()) continue;
+                    String oldBaseStr = getCellString(row.getCell(5));
+                    if (oldBaseStr.isEmpty()) continue;
 
-                String newBaseStr = getCellString(row.getCell(6));
-                if (newBaseStr.isEmpty()) newBaseStr = "0";
+                    String newBaseStr = getCellString(row.getCell(6));
+                    if (newBaseStr.isEmpty()) newBaseStr = "0";
 
-                String newMinStr = getCellString(row.getCell(7));
-                if (newMinStr.isEmpty()) newMinStr = "0";
+                    String newMinStr = getCellString(row.getCell(7));
+                    if (newMinStr.isEmpty()) newMinStr = "0";
 
-                String newMaxStr = getCellString(row.getCell(8));
-                if (newMaxStr.isEmpty()) newMaxStr = "0";
+                    String newMaxStr = getCellString(row.getCell(8));
+                    if (newMaxStr.isEmpty()) newMaxStr = "0";
 
-                if (newMinStr.equals(newMaxStr) && newMinStr.equals(newBaseStr)) continue;
+                    if (newMinStr.equals(newMaxStr) && newMinStr.equals(newBaseStr)) continue;
 
-                postList.add(
-                        new FcdSellChangePostDto(
-                                idStr,
-                                name,
-                                marketPriceStr,
-                                oldMinStr,
-                                oldMaxStr,
-                                oldBaseStr,
-                                newMinStr,
-                                newMaxStr,
-                                newBaseStr
-                        )
-                );
+                    postList.add(
+                            new FcdSellChangePostDto(
+                                    idStr,
+                                    name,
+                                    marketPriceStr,
+                                    oldMinStr,
+                                    oldMaxStr,
+                                    oldBaseStr,
+                                    newMinStr,
+                                    newMaxStr,
+                                    newBaseStr
+                            )
+                    );
+                }
             }
         }
         return postList;
